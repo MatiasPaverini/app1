84b5637a5ec6d1d5e3749f9d8f21e511a597c2f8
Matias Paverini <matias.paverini@tsoftlatam.com>
2020-08-27 22:37:17 -0300

    ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app1';
+  //title = 'app1';
+
+  edadUno;
+
+  edadDos;
+
+  resultadoEdad;
+
+  edadUno = document.getElementById('edadUno');
+
+  edadDos = document.getElementById('edadDos')
 }
