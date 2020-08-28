181bfb0539983adb85f5e6a32a40153da33ff84f
Matias Paverini <matias.paverini@tsoftlatam.com>
2020-08-27 21:46:33 -0300

    Ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentesComponent } from './componentes/componentes.component';
+import { BienvenidaComponent } from "./componentes/bienvenida/bienvenida.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentesComponent,
+    BienvenidaComponent
   ],
   imports: [
     BrowserModule,
